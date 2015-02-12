e6436a2f7464a3c3ac69928bb4bc0b3d16fa0ab4
Kostas Minaidis <kostas@creativecollective.gr>
2015-02-12 20:16:21 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # node-proxy-filter
 A Node.JS proxy server for injecting local resources on remote websites. 
+
+## DESCRIPTION
+
+Testing remote websites and replacing resources such as .js, .css files,
+image, etc. with local files.
+
+This script opens up local static server, which serves files from the
+current working directory. It also opens up a proxy server on port 8080
+(this can be changed) which replaces remote files with the ones found 
+in the local directory.
+
+## INSTALLATION
+
+$ chmod +x proxy.js
+$ mv proxy.js /usr/local/bin/proxy
+
+## USAGE
+
+```bash
+$ proxy
+```
+
+The above command will start a proxy server on port 8080.
+When the user visits a site via this proxy server, the script will replace
+all the remote requested files with the ones found in the local directory.
+
+```
+$ proxy jquery.js
+```
+
+In the above command we specify the files we want our proxy server 
+to look for in the requests and replace them. These files must reside 
+in the current directory.
+
+## TO DO
+* Implement HTTPS
---
+++ proxy.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+"use strict";
+
+// CORE MODULES
+
+var port        = 8080;
+var path        = require('path');
+var url         = require('url');
+var fs          = require('fs');
+var http        = require('http');
+
+// NPM PACKAGES
+
+var util        = require('util');
+var colors      = require('colors');
+var httpProxy   = require('http-proxy');
+var connect     = require('connect');
+var serveStatic = require('serve-static');
+
+// CONFIGURATION
+
+var localPort  = 9000;
+var localhost  = "http://localhost:" + localPort + "/";
+var proxy      = httpProxy.createProxyServer({});
+var currentDir = process.cwd();
+var files      = [];
+var target;
+
+/*====== CREATE FILE REPLACEMENT LIST ======*/
+
+if ( process.argv.length > 2 ){
+
+  // PRODUCE FILE REPLACEMENT LIST FROM COMMAND LINE ARGUMENTS
+  var args = process.argv.slice(2);
+  process.argv.slice(2).forEach(function (fileName) {
+    files.push( fileName );
+  });
+
+} else {
+
+  // PRODUCE FILE REPLACEMENT LIST FROM CURRENT DIR
+  files = fs.readdirSync(currentDir);
+
+}
+
+/*====== CREATE PROXY SERVER ======*/
+
+var server = http.createServer(function(req, res) {
+
+  if ( files.indexOf(path.basename(req.url)) > -1 ){
+
+    req.url = localhost + path.basename( req.url );
+    target  = localhost;
+
+  } else {
+
+    target = 'http://' + req.headers.host;
+
+  }
+
+  proxy.web( req, res, { target: target }); 
+
+}).listen( port, function(){
+
+  util.puts('HTTP PROXY SERVER'.red + ' LISTENING ON PORT: '.green.bold + port.toString().yellow);
+
+});
+
+/*====== STATIC SERVER : SERVING REPLACEMENT FILES ======*/
+
+var app        = connect();
+    app.use(serveStatic(currentDir));
+
+http.createServer(app).listen( localPort, function(){
+
+  util.puts('LOCAL STATIC HTTP SERVER'.red + ' SERVING ON PORT: '.green.bold + localPort.toString().yellow);
+
+});
+
+/* proxy.close(); */
